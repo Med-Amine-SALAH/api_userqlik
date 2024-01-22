6474d16a44e3cf94a62766cb96fad1cafbab19aa
Med-Amine-Salah <aminesalah023@gmail.com>
2024-01-22 17:46:52 +0100

    qlik

--- api_userqlik.py
+++ api_userqlik.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 csv_file_path = 'usernames.csv'
 try:
-    with open(csv_file_path, 'x', newline='') as csvfile:
-        fieldnames = ['username']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+    with open(csv_file_path, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['username'])
 except FileExistsError:
     pass
+
 
 def is_username_exists(username):
     with open(csv_file_path, 'r', newline='') as csvfile:
