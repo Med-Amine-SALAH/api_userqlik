e51066c51744f763539924e5b9700371117847fd
Med-Amine-Salah <aminesalah023@gmail.com>
2024-01-22 15:57:16 +0100

    qlik

--- api_userqlik.py
+++ api_userqlik.py
@@ -1,13 +1,11 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/get_username', methods=['GET'])
 def get_username():
-    
     username = request.args.get('username')
-    return f'Hello, {username}!'
+    return render_template('accueil.html', username=username)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
